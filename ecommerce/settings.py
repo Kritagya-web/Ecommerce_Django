eba75e891179484fda5a0d5fe7cf2e73e898e8df
Kumra <kkumra2@calstatela.edu>
2025-01-10 10:23:44 -0800

    Changes to make deployment work

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,8 +27,10 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
