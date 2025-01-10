cf7ee4a29453d60c02d9b122b3dae7473239acf1
Kumra <kkumra2@calstatela.edu>
2025-01-10 12:25:55 -0800

    Changes to make deployment work

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+echo "Starting pip install"
 pip install -r requirements.txt
-python3.10 manage.py collectstatic
+echo "Starting collectstatic"
+python3.9 manage.py collectstatic --noinput
